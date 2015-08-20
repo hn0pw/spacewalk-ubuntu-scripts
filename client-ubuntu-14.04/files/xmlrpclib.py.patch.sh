b9510a1ff78c51a07bd4a05ecdf08cd3a3be6f6a
root <root@ubuntu-template.intern.filmwords.com>
2015-08-20 14:41:45 +0200

    update

---
+++ client-ubuntu-14.04/files/xmlrpclib.py.patch.sh
@@ -0,0 +1,2 @@
+diff --binary -uN xmlrpclib.py.org xmlrpclib.py.mod > ../patch_python2.7.diff
+#diff --binary -uN /usr/share/rhn/up2date_client/xmlrpclib.py xmlrpclib.py.mod > ../patch_python2.7.diff
