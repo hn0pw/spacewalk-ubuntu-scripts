c17243db3322b3dd4a354b21d4721bda0074179a
root <root@ubuntu-template.intern.filmwords.com>
2015-08-20 14:26:36 +0200

    update

---
+++ client-ubuntu-14.04/files/debUtils.py.patch.sh
@@ -0,0 +1,2 @@
+diff --binary -uN debUtils.py.org debUtils.py.mod > ../patch_spacewalk_ubuntu_package_version.diff
+#diff --binary -uN /usr/share/rhn/up2date_client/debUtils.py debUtils.py.mod > ../patch_spacewalk_ubuntu_package_version.diff
