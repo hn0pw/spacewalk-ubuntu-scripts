ba81f580a8a5aa7cd82e16a2a4603c6c58d303dc
Ramon Gamma <gamma@filmwords.com>
2015-08-21 08:58:40 +0200

    Change the interval

--- client-ubuntu-14.04/install.sh
+++ client-ubuntu-14.04/install.sh
@@ -15,6 +15,10 @@ echo "Your activation key: "$SPACEWALK_ACTIVATION_KEY
 apt-get update
 apt-get -y install apt-transport-spacewalk rhnsd python-libxml2
 
+# Specify the interval that rhnsd should wait between checking the Red Hat Network.
+# Default is 240 minutes, the minimum is  60  minutes.
+sed -i 's/INTERVAL=240/INTERVAL=60/g' /etc/rhn/rhnsd
+
 # Fix errors emails from spacewalk
 # https://github.com/ahakala/deb-spacewalk
 FILE="/etc/apt/apt.conf"
@@ -36,6 +40,8 @@ echo "Register this client with your spacewalk host"
 echo "rhnreg_ks --serverUrl=https://"$SPACEWALK_HOST"/XMLRPC --sslCACert=/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT --activationkey="$SPACEWALK_ACTIVATION_KEY
 wget http://$SPACEWALK_HOST/pub/RHN-ORG-TRUSTED-SSL-CERT -O /usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT
 rhnreg_ks --serverUrl=https://$SPACEWALK_HOST/XMLRPC --sslCACert=/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT --activationkey=$SPACEWALK_ACTIVATION_KEY
+
+/etc/init.d/rhnsd restart
 
 echo "Finished"
 echo "Please go to your spacewalk web interface to see this client status"
